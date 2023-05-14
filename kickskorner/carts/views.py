417cfa7013ec339394c70ced222b4d90c489d879
Suman Sapkota <sumansapkota0007@gmail.com>
2023-05-14 10:33:32 +0545

    update

--- kickskorner/carts/views.py
+++ kickskorner/carts/views.py
@@ -98,7 +98,7 @@ def remove_cart(request, product_id, cart_item_id):
 def remove_cart_item(request, product_id, cart_item_id):
     cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product, id=product_id)
-    cart_item = CartItem.objects.get(product=product, cart=cart, id = cart_item_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart, id =cart_item_id)
     cart_item.delete()
     return redirect('cart')
 
