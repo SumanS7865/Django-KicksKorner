6714c6d7c2ec4def208f6f091cd45f76e467e3c5
Suman Sapkota <sumansapkota0007@gmail.com>
2023-05-16 09:33:54 +0545

    update

--- kickskorner/kickskorner/settings.py
+++ kickskorner/kickskorner/settings.py
@@ -147,8 +147,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 #smtp configuration
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sumansapkota0007@gmail.com'
-EMAIL_HOST_PASSWORD = 'matrixisalie'
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 EMAIL_USE_TLS = True
