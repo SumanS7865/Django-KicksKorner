829fcee43f36fef78b3915395831b4b538d6997a
Suman Sapkota <sumansapkota0007@gmail.com>
2023-05-17 19:17:45 +0545

    update

--- kickskorner/customadmin/urls.py
+++ kickskorner/customadmin/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .import views
+from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name="customadmin"),
-    path('login/', views.admin_login, name="admin_login"),
-    path('register/', views.admin_register, name="admin_register"),
-
-] 
+    path("", views.admin_login, name="admin_login"),
+    path("dashboard", views.dashboard, name="customadmin"),
+    path("error404/", views.error404, name="error404"),
+    path("logout/", views.admin_logout, name="admin_logout"),
+    path("products/", views.products, name="products"),
+]
--- kickskorner/customadmin/views.py
+++ kickskorner/customadmin/views.py
@@ -1,13 +1,50 @@
 from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+
 
 # Create your views here.
 
 
 def dashboard(request):
-    return render (request, 'customadmin/dashboard.html')
+    return render(request, "customadmin/dashboard.html")
+
 
 def admin_login(request):
-    return render (request, 'customadmin/login.html')
+    if request.method == "POST":
+        email = request.POST["email"]
+        password = request.POST["password"]
+
+        user = auth.authenticate(email=email, password=password)
+
+        if user and user.is_superadmin:
+            auth.login(request, user)
+            # messages.success(request, 'Your are now logged in.')
+            return redirect("customadmin")
+        else:
+            messages.error(request, "invalid login credentials")
+            return redirect("admin_login")
+
+    return render(request, "customadmin/login.html")
+
+
+@login_required(login_url="admin_login")
+def admin_logout(request):
+    auth.admin_logout(request)
+    messages.success(request, "you are logged out.")
+    return redirect("admin_login")
+
 
 def admin_register(request):
-    return render (request, 'customadmin/register.html')
+    return render(request, "customadmin/register.html")
+
+
+def error404(request):
+    return render(request, "customadmin/error404.html")
+
+
+def products(request):
+    return render(request, "customadmin/products.html")
