531d9da8909c9beb9611d3c58abaabcd2802cd86
Suman Sapkota <sumansapkota0007@gmail.com>
2023-05-19 00:03:20 +0545

    update

--- kickskorner/carts/models.py
+++ kickskorner/carts/models.py
@@ -3,23 +3,24 @@ from app.models import Product, Variation
 
 # Create your models here.
 
+
 class Cart(models.Model):
-    cart_id     = models.CharField(max_length=250, blank=True)
-    date_added  = models.DateField(auto_now_add=True)
+    cart_id = models.CharField(max_length=250, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.cart_id
-    
+
 
 class CartItem(models.Model):
-    product     = models.ForeignKey(Product, on_delete=models.CASCADE)
-    variations  = models.ManyToManyField(Variation, blank=True)
-    cart        = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    quantity    = models.IntegerField()
-    is_active   = models.BooleanField(default=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variations = models.ManyToManyField(Variation, blank=True)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    is_active = models.BooleanField(default=True)
 
     def sub_total(self):
-        return (self.product.selling_price * self.quantity)
+        return self.product.selling_price * self.quantity
 
     def __unicode__(self):
         return self.product
--- kickskorner/myadmin/views.py
+++ kickskorner/myadmin/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.http import HttpResponseRedirect
-from app.models import Product
+from app.models import Product, Variation
+from carts.models import Cart, CartItem
+from category.models import *
+from members.models import *
+from django.utils.text import slugify
 
 
 # Create your views here.
@@ -45,28 +49,91 @@ def products(request):
     return render(request, "myadmin/products.html", {"products": products})
 
 
+def addproduct(request):
+    if request.method == "POST":
+        # Process form submission
+        product_name = request.POST["product_name"]
+        slug = slugify(product_name)
+        description = request.POST["description"]
+        selling_price = request.POST["selling_price"]
+        discounted_price = request.POST["discounted_price"]
+        product_detail_ribbon = request.POST["product_detail_ribbon"]
+        ribbon = request.POST["ribbon"]
+        image1 = request.FILES["image1"]
+        image2 = request.FILES["image2"]
+        image3 = request.FILES["image3"]
+        image4 = request.FILES["image4"]
+        image5 = request.FILES["image5"]
+        image6 = request.FILES["image6"]
+        stock = request.POST["stock"]
+        category_id = request.POST["category"]
+
+        category = get_object_or_404(
+            Category, id=category_id
+        )  # Retrieve the Category instance
+
+        product = Product(
+            product_name=product_name,
+            slug=slug,
+            description=description,
+            selling_price=selling_price,
+            discounted_price=discounted_price,
+            product_detail_ribbon=product_detail_ribbon,
+            ribbon=ribbon,
+            image1=image1,
+            image2=image2,
+            image3=image3,
+            image4=image4,
+            image5=image5,
+            image6=image6,
+            stock=stock,
+            category=category,
+        )
+        product.save()
+
+        messages.success(request, "Product added successfully.")
+        return redirect("addproduct")
+    return render(request, "myadmin/addproduct.html")
+
+
 def variations(request):
-    return render(request, "myadmin/variations.html")
+    variations = Variation.objects.all()
+    context = {
+        "variations": variations,
+    }
+    return render(request, "myadmin/variations.html", context)
 
 
 def cartitems(request):
-    return render(request, "myadmin/cartitems.html")
+    cartitems = CartItem.objects.all()
+    context = {
+        "cartitems": cartitems,
+    }
+    return render(request, "myadmin/cartitems.html", context)
 
 
 def carts(request):
-    return render(request, "myadmin/carts.html")
+    carts = Cart.objects.all()
+    context = {
+        "carts": carts,
+    }
+    return render(request, "myadmin/carts.html", context)
 
 
 def categories(request):
-    return render(request, "myadmin/categories.html")
+    categories = Category.objects.all()
+    context = {
+        "categories": categories,
+    }
+    return render(request, "myadmin/categories.html", context)
 
 
 def accounts(request):
-    return render(request, "myadmin/accounts.html")
-
-
-def addproduct(request):
-    return render(request, "myadmin/addproduct.html")
+    accounts = Account.objects.all()
+    context = {
+        "accounts": accounts,
+    }
+    return render(request, "myadmin/accounts.html", context)
 
 
 def addvariation(request):
